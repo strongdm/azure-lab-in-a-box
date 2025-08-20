fe11f5c57f85f2f0732b5ee79a7991a749e031e7
Nicolas Corrarello <nicolas@corrarello.com>
2025-08-20 10:47:56 +0100

    Migrating from simple server to flexible server for PostgreSQL (#9)

--- postgresql/output.tf
+++ postgresql/output.tf
@@ -1,5 +1,5 @@
 output "fqdn" {
-    value = azurerm_postgresql_server.server.fqdn
+    value = azurerm_postgresql_flexible_server.server.fqdn
 }
 
 output "thistagset" {
--- postgresql/psql.tf
+++ postgresql/psql.tf
@@ -10,33 +10,26 @@ resource "random_pet" "azurerm_postgresql_server_name" {
 }
 
 // Create a PostgreSQL server in Azure
-resource "azurerm_postgresql_server" "server" {
+resource "azurerm_postgresql_flexible_server" "server" {
   name                = random_pet.azurerm_postgresql_server_name.id
   location            = var.region
   resource_group_name = var.rg
   administrator_login          = var.target_user
-  administrator_login_password = local.admin_password
+  administrator_password = local.admin_password
+  public_network_access_enabled = true
 
   // Basic tier with minimal resources for lab/demo purposes
-  sku_name   = "B_Gen5_1"
-  version    = "11"
-  storage_mb = 5120
-
-  infrastructure_encryption_enabled = false
-
-  public_network_access_enabled    = true
-  ssl_enforcement_enabled          = true
-  ssl_minimal_tls_version_enforced = "TLS1_2"
-  geo_redundant_backup_enabled = false
+  sku_name   = "B_Standard_B2s"
+  version    = "15"
+  storage_mb = 32768
 
   tags = local.thistagset
 }
 
 // Firewall rule to allow access from the StrongDM relay
-resource "azurerm_postgresql_firewall_rule" "allowgw" {
+resource "azurerm_postgresql_flexible_server_firewall_rule" "allowgw" {
   name                = "AllowGatewaysAndRelays"
-  server_name         = azurerm_postgresql_server.server.name
-  resource_group_name = var.rg
+  server_id         = azurerm_postgresql_flexible_server.server.id
   start_ip_address    = var.relay_ip
   end_ip_address      = var.relay_ip
 }
@@ -44,7 +37,7 @@ resource "azurerm_postgresql_firewall_rule" "allowgw" {
 // Store PostgreSQL username in Azure Key Vault
 resource "azurerm_key_vault_secret" "psql-username" {
   name         = "${var.name}-psql-username"
-  value        = "${var.target_user}@${random_pet.azurerm_postgresql_server_name.id}"
+  value        = "${var.target_user}"
   key_vault_id = var.key_vault_id
   tags = local.thistagset
 }
